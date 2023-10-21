1f8b0307ee608138d9399a7e339e7cef4f7f92ff
lggomez18 <137099040+lggomez18@users.noreply.github.com>
2023-10-21 09:29:41 -0500

    add current timezones

---
+++ src/app.js
@@ -0,0 +1,25 @@
+setInterval (function () {
+//Los Angeles
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = document.querySelector (".dateLA");
+let losAngelesTimeElement = document.querySelector (".timeLA");
+let losAngelesTime= moment().tz("America/Los_Angeles");
+losAngelesDateElement.innerHTML = losAngelesTime. format("MMMM Do YYYY");
+losAngelesTimeElement.innerHTML = 
+`${losAngelesTime. format("h:mm:ss")}
+<small>${losAngelesTime.format("A")}</small>`;
+
+}, 1000);
+
+
+setInterval (function () {
+//Paris
+let parisElement = document.querySelector("#paris");
+let parisDateElement = document.querySelector (".dateFR");
+let parisTimeElement = document.querySelector (".timeFR");
+let parisTime= moment().tz("Europe/Paris");
+parisDateElement.innerHTML = parisTime. format("MMMM Do YYYY");
+parisTimeElement.innerHTML = 
+`${parisTime. format("h:mm:ss")}
+<small>${parisTime.format("A")}</small>`;
+}, 1000);
